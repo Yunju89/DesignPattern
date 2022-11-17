dde441bca710a1dafed6406db8da7c8a24fae664
Yunju <lyj@gaeasoft.co.kr>
2022-11-17 09:14:07 +0900

    주석수정

--- app/src/main/java/com/example/designpattern/observer/NumberGenerator.java
+++ app/src/main/java/com/example/designpattern/observer/NumberGenerator.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class NumberGenerator {
     private final ArrayList observerList = new ArrayList();      // Observer 목록 저장 할 ArrayList
 
-    private Random random = new Random();               // 난수 생성
+    private Random random = new Random();                        // 난수 생성
     private int number;
 
     public void addObserver(@NonNull Observer observer) {        // Observer 추가
@@ -25,11 +25,11 @@ public class NumberGenerator {
 
         while (it.hasNext()) {                          // 다음 읽어올 요소가 있는 동안 꺼내오기
             Observer observer = (Observer) it.next();   // Object obj = it.next();  -> 요소 하나씩 꺼내는 방법
-            observer.update(number);              // Observer 클래스의 update 사용하여 NumberGenerator 변경 값을 알림
+            observer.update(number);              // Observer 클래스의 update 사용하여 Number 값을 알림
         }
     }
 
-    public void execute() {                            // 숫자 생성
+    public void execute() {                            // 실행
         for (int i = 0; i < 10; i++) {                 // 10번 반복
             number = random.nextInt(50);         // 0~50까지, int 형 파라미터를 입력하면, 0에서 그 파라미터 미만의 랜덤한 정수를 반환한다.
             notifyObservers();                          // 갱신
